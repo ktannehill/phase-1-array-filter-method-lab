824c32bd836fe0f423063c81da8b88bbc54f90e0
Kat Tannehill <katetannehill@gmail.com>
2023-09-16 14:45:27 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+const findMatching = function(driversArr, str) {
+    let driversMatch = driversArr.filter(driver => {
+        return driver.toLowerCase().indexOf(str.toLowerCase()) > -1
+    })
+    return driversMatch
+}
+
+// const redToys = toys.filter(function (toy) { 
+    // 	return toy.toLowerCase().indexOf('red')  
+    // 		> -1; 
+    // }); 
+    
+const fuzzyMatch = function(driversArr, subStr) {
+    let driversMatch = driversArr.filter(driver => {
+        return driver.toLowerCase().indexOf(subStr.toLowerCase()) === 0
+    })
+    return driversMatch
+}
+
+const matchName = function(driversArr, str) {
+    let driversMatch = driversArr.filter(driver => {
+        return driver["name"] === str
+    })
+    return driversMatch
+}
+
+
+
